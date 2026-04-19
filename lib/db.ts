e8a6f329eac3f60402d11a1af1c835b9dcac022d
oceankesarwani <ocean.kesarwani@gmail.com>
2026-04-19 21:23:49 +0530

    fix/db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import * as PrismaModule from "@prisma/client";
 
 // Prevent multiple instances of Prisma Client in development
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaModule.PrismaClient | undefined;
 }
 
 // Do not instantiate Prisma immediately because Turbopack 
 // pre-evaluates modules in an Edge sandbox, causing crashes.
-export const prisma = new Proxy({} as PrismaClient, {
+export const prisma = new Proxy({} as PrismaModule.PrismaClient, {
   get: (target, prop) => {
     if (!global.prisma) {
-      global.prisma = new PrismaClient({ log: ["query"] });
+      global.prisma = new PrismaModule.PrismaClient({ log: ["query"] });
     }
     return (global.prisma as any)[prop];
   }
